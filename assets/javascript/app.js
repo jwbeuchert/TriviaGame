4a4454f019472608b54f09c7e15e1338d01a561e
Jacob Beuchert <jacobbeuchert@gmail.com>
2019-09-18 16:07:06 -0600

    Adding inital javascript code.

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// Creating Objects, storing certain variables
+var player = {
+    isPlaying: false,
+    isWaiting: false,
+    numberCorrect: 0,
+    numberIncorrect: 0,
+    wins: 0,
+    losses: 0
+  }
+
+  
