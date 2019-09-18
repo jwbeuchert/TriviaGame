d03977ca1dd967b8c53f889c7aa583908ae1c788
Jacob Beuchert <jacobbeuchert@gmail.com>
2019-09-18 16:09:04 -0600

    Adding initial timer settings.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,4 +8,15 @@ var player = {
     losses: 0
   }
 
-  
+   // Setting up an empty array for questions, the current question, and the status of the game (timer)
+   var questionsArray = [];
+   var currentQuestion;
+   var gameStatus = [];
+
+     // timer variables.  timer = variable for javascript timer.  time = variables for actual time.
+  var questionTimer;
+  var summaryTimer;
+  var timeQuestion = 30000;
+  var timeSummary = 10000;
+  var time = 0;
+  var counter;
